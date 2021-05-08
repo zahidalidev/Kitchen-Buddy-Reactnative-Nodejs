fc91bad795fb4a8aaae6b4fc48b14bec7d902a49
zahidalidev <m.zahidalidev@gmail.com>
2021-05-08 07:30:02 +0500

    home screen top container

--- Kitchen-Buddy-reactnative/app/screens/Home.js
+++ Kitchen-Buddy-reactnative/app/screens/Home.js
@@ -6,7 +6,7 @@ import { RFPercentage } from 'react-native-responsive-fontsize';
 
 import AppTextInput from '../components/AppTextInput';
 import colors from '../config/colors';
-import banner from "../../assets/images/groceryBanner_small.jpg"
+import banner from "../../assets/images/order-grocery-small.jpg"
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -23,20 +23,23 @@ function Home(props) {
             <StatusBar style="light" backgroundColor={colors.primaryLight} />
 
             {/* Kitchen buddy top container */}
-            <ImageBackground style={{ width: windowWidth, height: RFPercentage(30), flex: 0.8, flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-end' }} source={banner} >
-                <View style={{ flexDirection: 'column' }} >
-                    {/* Text feilds */}
-                    <View style={{ width: "85%", bottom: RFPercentage(9) }} >
-                        <AppTextInput
-                            placeHolder="Search"
-                            width="100%"
-                            value={searchValue}
-                            onChange={(text) => setSearchValue(text)}
-                            icon="magnify"
-                        />
+            <View style={{ backgroundColor: colors.primary, flex: 0.7, justifyContent: 'flex-end' }} >
+                <ImageBackground style={{ marginBottom: RFPercentage(1), width: windowWidth, height: RFPercentage(30), alignItems: 'center', justifyContent: 'flex-end' }} source={banner} >
+                    <View style={{ flexDirection: 'column' }} >
+                        {/* Text feilds */}
+                        <View style={{ width: "85%", bottom: RFPercentage(7.5) }} >
+                            <AppTextInput
+
+                                placeHolder="Search"
+                                width="100%"
+                                value={searchValue}
+                                onChange={(text) => setSearchValue(text)}
+                                icon="magnify"
+                            />
+                        </View>
                     </View>
-                </View>
-            </ ImageBackground>
+                </ ImageBackground>
+            </View>
 
             {/* Bottom Contaienr */}
             <View style={{ marginTop: -RFPercentage(7), borderTopLeftRadius: RFPercentage(8), backgroundColor: colors.lightGrey, width: "100%", flex: 1.8, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} >
