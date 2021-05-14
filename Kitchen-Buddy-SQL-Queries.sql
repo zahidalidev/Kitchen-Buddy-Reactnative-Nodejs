15f0d5a5580902783e1e8a2d47b6a4d6fd76b11a
zahidalidev <m.zahidalidev@gmail.com>
2021-05-14 15:38:57 +0500

    bacend packages installed

---
+++ Kitchen-Buddy-SQL-Queries.sql
@@ -0,0 +1,42 @@
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE database kitchenBuddy;
+USE kitchenBuddy;
+
+-- -----------------------------------------------------
+-- Table user
+-- -----------------------------------------------------
+CREATE TABLE customer (
+  id INT NOT NULL IDENTITY,
+  name VARCHAR(155) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+)
+
+
+-- -----------------------------------------------------
+-- Table ingredient
+-- -----------------------------------------------------
+CREATE TABLE ingredient (
+  id INT NOT NULL IDENTITY,
+  name VARCHAR(255) NOT NULL,
+  brandName VARCHAR(255) NULL,
+  category VARCHAR(255) NULL,
+  location VARCHAR(255) NULL,
+  confectionType VARCHAR(255) NULL,
+  ripeness VARCHAR(255) NULL,
+  ripenessEditedDate DATE NULL,
+  lastCheckDate DATE NULL,
+  frozen VARCHAR(45) NULL,
+  openClose VARCHAR(45) NULL,
+  expirationDate DATE NULL,
+  PRIMARY KEY (id),
+  userId INT NOT NULL foreign key references customer(id)
+)
